e26236ed69c4e360441642a7d99deb72d08fa3f0
85pando <85pando@googlemail.com>
2014-05-23 11:34:53 +0200

    reconfigured some aliases i don't want

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,14 +1,14 @@
 # Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
+#alias pu='pushd'
+#alias po='popd'
 
 # Basic directory operations
 alias ...='cd ../..'
 alias -- -='cd -'
 
 # Super user
-alias _='sudo'
-alias please='sudo'
+#alias _='sudo'
+#alias please='sudo'
 
 #alias g='grep -in'
 
@@ -30,7 +30,7 @@ alias lsa='ls -lah'
 alias l='ls -lah'
 alias ll='ls -lh'
 alias la='ls -lAh'
-alias sl=ls # often screw this up
+#alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
 
